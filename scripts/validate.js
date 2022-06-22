159613c5b07a6de01e27ad536d37cd81e062b7b1
Maxim Zhosan <maxim.zhosan@gmail.com>
2022-06-22 12:00:07 +0300

    bugfix: Исправление ошибок JS

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,38 +1,24 @@
-const par = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__error_visible'
-}
-
-function enableValidation() {
-    const formList = Array.from(document.querySelectorAll(`${par.formSelector}`));
-    formList.forEach((formElement) => setEventListeners(formElement));
-};
-
-const setEventListeners = (formElement) => {
-    const inputList = Array.from(formElement.querySelectorAll(`${par.inputSelector}`));
-    const buttonElement = formElement.querySelector(`${par.submitButtonSelector}`);
+const setEventListeners = (formElement, par) => {
+    const inputList = Array.from(formElement.querySelectorAll(par.inputSelector));
+    const buttonElement = formElement.querySelector(par.submitButtonSelector);
     // чтобы проверить состояние кнопки в самом начале
-    toggleButtonState(inputList, buttonElement);
+    toggleButtonState(inputList, buttonElement, par);
 
     inputList.forEach((inputElement) => {
         inputElement.addEventListener('input', function () {
-            checkInputValidity(formElement, inputElement);
+            checkInputValidity(formElement, inputElement, par);
             // чтобы проверять его при изменении любого из полей
-            toggleButtonState(inputList, buttonElement);
+            toggleButtonState(inputList, buttonElement, par);
         });
     });
 };
 
-const toggleButtonState = (inputList, buttonElement) => {
+const toggleButtonState = (inputList, buttonElement, par) => {
     if (hasInvalidInput(inputList)) {
-        buttonElement.classList.add(`${par.inactiveButtonClass}`)
+        buttonElement.classList.add(par.inactiveButtonClass)
     }
     else {
-        buttonElement.classList.remove(`${par.inactiveButtonClass}`)
+        buttonElement.classList.remove(par.inactiveButtonClass)
     }
 }
 
@@ -42,26 +28,39 @@ const hasInvalidInput = (inputList) => {
     })
 }
 
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement, par) => {
     if (!inputElement.validity.valid) {
-        showInputError(formElement, inputElement, inputElement.validationMessage);
+        showInputError(formElement, inputElement, inputElement.validationMessage, par);
     } else {
-        hideInputError(formElement, inputElement);
+        hideInputError(formElement, inputElement, par);
     }
 };
 
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage, par) => {
     const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-    inputElement.classList.add(`${par.inputErrorClass}`);
+    inputElement.classList.add(par.inputErrorClass);
     errorElement.textContent = errorMessage;
-    errorElement.classList.add(`${par.errorClass}`);
+    errorElement.classList.add(par.errorClass);
 };
 
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, par) => {
     const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-    inputElement.classList.remove(`${par.inputErrorClass}`);
-    errorElement.classList.remove(`${par.errorClass}`);
+    inputElement.classList.remove(par.inputErrorClass);
+    errorElement.classList.remove(par.errorClass);
     errorElement.textContent = '';
 };
 
-enableValidation(); 
+function enableValidation(par) {
+    const formList = Array.from(document.querySelectorAll(par.formSelector));
+    formList.forEach((formElement) => setEventListeners(formElement, par));
+};
+
+enableValidation({
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
+  }); 
+
